cdc7a1ae0695be0fe8621067563c8f22ee491753
Rolf <rolf.bense@web.de>
2026-03-15 18:05:54 +0100

    update

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def main():
     with st.sidebar:
         # Ein kleines Logo oder Icon oben in der Sidebar macht sich gut
         st.title("🌌 Navigation")
-        page = st.selectbox("Wähle eine Seite:", ["Analyse", "Über das Projekt"]
+        page = st.selectbox("Wähle eine Seite:", ["Analyse", "Über das Projekt"],
                             label_visibility="collapsed"
                             )
         st.divider()
